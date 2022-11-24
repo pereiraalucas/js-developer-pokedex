8d04d40655ff49aa563c39cf0c444ff19b085f70
pereiraalucas <lucas.antonio.pinto.pereira@gmail.com>
2022-11-24 14:31:55 -0300

    commit inicial

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,35 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
+
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("btn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function () {
+            modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+
 const maxRecords = 151
-const limit = 10
+const limit = 20
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
